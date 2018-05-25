2a6332bd746dbcf4446edc6cf6bd7274de61f4be
Jeff Chiu <shadow3x3x3@gmail.com>
2018-05-25 16:02:36 +0800

    Add detail query (WIP)

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = 'https://ppstrq.nat.gov.tw/pps/pubQuery/PropertyQuery/propertyQuery.do'
-debtor_name = '統一'
+BASE_URL = 'https://ppstrq.nat.gov.tw/pps/pubQuery/PropertyQuery/propertyQuery.do'
+DETAIL_URL = 'https://ppstrq.nat.gov.tw/pps/pubQuery/PropertyQuery/propertyDetail.do'
 
+DETAIL_SYMBOL = 'onclick'
+debtor_name = '大同'
+
+# TODO: Make all strings of key to Constant, just like URL (Performance)
 param = {
   'method': 'query',
   'regUnitCode': '',
@@ -19,13 +23,52 @@ param = {
   'creditorTypeRadio': 1,
   'queryCreditorName': '',
   'queryCreditorNo': '',
-  'struts.token.name': 'struts.token',
-  'struts.token': '9BJIP8NRCRJMTJT7O6ZDALD2IZZPR5TB',
-  'monthCount': 546800,
-  'totalCount': 34857941
 }
 
-req = requests.post(URL, data=param)
 
-soup = BeautifulSoup(req.content)
-print(soup.prettify())
+responses = []
+
+base_req = requests.post(BASE_URL, data=param)
+
+base_soup = BeautifulSoup(base_req.content, 'lxml')
+
+reg_unit_codes = []
+certificate_words = []
+
+# Get all reg unit codes and certificate words for getting detail data
+# TODO: Just get single page here, fetch all pages by urself =) (Feature)
+for tr in base_soup.tbody.find_all('tr')[:1]:
+    detail_info = tr[DETAIL_SYMBOL][9:-1].replace("'", '').split(',')
+    reg_unit_codes.append(int(detail_info[0]))
+    certificate_words.append(detail_info[1])
+
+# print(reg_unit_codes)
+# print(certificate_words)
+
+# Get detail data
+for code, word in zip(reg_unit_codes, certificate_words):
+    basic_response = {
+        '債務人(買受人、受託人)名稱': '',
+        '契約啟始日期': '',
+        '契約終止日期': '',
+        '抵押權人(出賣人、信託人)名稱': '',
+        '擔保債權金額': '',
+        '案件類別': '',
+        '標的物種類': '',
+    }
+
+    print(word)
+    param['regUnitCode'] = code
+    param['certificateAppNoWord'] = word
+
+    detail_req = requests.post(DETAIL_URL, data=param)
+    detail_soup = BeautifulSoup(detail_req.content, 'lxml')
+
+    target_divs = detail_soup.find_all('div', {
+        'class': 'well pubDetailWellHalf center-block'})
+    basic_response['債務人(買受人、受託人)名稱'] = target_divs[0].find('div', {'class': 'row'}).text[4:].strip()
+    basic_response['抵押權人(出賣人、信託人)名稱'] = target_divs[1].find('div', {'class': 'row'}).text[4:].strip()
+    responses.append(basic_response)
+
+
+print(responses)
